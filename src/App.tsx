3db7a19dcee102df3253fd19a283047f5bd63675
Santiago <sacasalv@gmail.com>
2026-01-17 20:19:07 +0100

    feat(ui): Añade footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { Card } from "./components/Card";
+import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-primary d-flex content-start justify-center min-h-screen p-4">
-      <Card />
+      <Hero />
       <Projects />
+      <Footer className="mt-auto fixed bottom-0 left-0 w-full"/>
     </div>
   );
 }
