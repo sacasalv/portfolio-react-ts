f55eab9c167fb28c1a52c425447e0785b6d800a5
sacasalv <sacasalv@gmail.com>
2026-01-29 21:31:48 +0100

    style(styles): Efecto de sacar movil y estilos de bordes

--- src/components/MobilePreview.tsx
+++ src/components/MobilePreview.tsx
@@ -4,12 +4,9 @@ export function MobilePreview() {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div
-      className={`fixed bottom-0 left-0 w-full h-96 transition-transform duration-700 ease-out cursor-pointer ${isExpanded ? 'translate-y-0' : 'translate-y-80'}`}
-      onClick={() => setIsExpanded(!isExpanded)}
-    >
-      <div className="border-8 rounded-t-lg border-black bg-[url(./../public/images/fondo-movil.jpg)] bg-center bg-origin-content bg-content bg-no-repeat w-2xs h-[500px] ms-40">
-        <div className="bg-white/1 backdrop-blur-[2px] h-full grid grid-cols-3 gap-2">
+      <div className={`fixed bottom-0 left-0 w-2xs h-[350px] ms-40 transition-transform duration-700 ease-out cursor-pointer ${isExpanded ? 'translate-y-0' : 'translate-y-80'} border-t-6 border-l-6 border-r-6 rounded-t-[60px] p-2 border-black bg-[url(./../public/images/fondo-movil.jpg)] bg-center bg-origin-content bg-content bg-no-repeat`} onClick={() => setIsExpanded(!isExpanded)}>
+        <div className="bg-white/1 backdrop-blur-[2px] h-full rounded-t-[30px] grid grid-cols-3 gap-2">
+        <div className="absolute top-0 left-21 bg-black border-black-100 rounded-2xl w-25 h-8"></div>
           <div className="mt-20">
             <img
               src="/public/iconos/instagram.png"
@@ -33,6 +30,5 @@ export function MobilePreview() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
