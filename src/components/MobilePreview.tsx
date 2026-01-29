fc478471b445bf584f6c2394aaacf47942437e78
sacasalv <sacasalv@gmail.com>
2026-01-29 21:36:18 +0100

    style(styles): Bordes menos redondos

--- src/components/MobilePreview.tsx
+++ src/components/MobilePreview.tsx
@@ -4,7 +4,7 @@ export function MobilePreview() {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-      <div className={`fixed bottom-0 left-0 w-2xs h-[350px] ms-40 transition-transform duration-700 ease-out cursor-pointer ${isExpanded ? 'translate-y-0' : 'translate-y-80'} border-t-6 border-l-6 border-r-6 rounded-t-[60px] p-2 border-black bg-[url(./../public/images/fondo-movil.jpg)] bg-center bg-origin-content bg-content bg-no-repeat`} onClick={() => setIsExpanded(!isExpanded)}>
+      <div className={`fixed bottom-0 left-0 w-2xs h-96 ms-40 transition-transform duration-700 ease-out cursor-pointer ${isExpanded ? 'translate-y-0' : 'translate-y-80'} border-t-6 border-l-6 border-r-6 rounded-t-[50px] p-2 border-black bg-[url(./../public/images/fondo-movil.jpg)] bg-center bg-origin-content bg-content bg-no-repeat`} onClick={() => setIsExpanded(!isExpanded)}>
         <div className="bg-white/1 backdrop-blur-[2px] h-full rounded-t-[30px] grid grid-cols-3 gap-2">
         <div className="absolute top-0 left-21 bg-black border-black-100 rounded-2xl w-25 h-8"></div>
           <div className="mt-20">
