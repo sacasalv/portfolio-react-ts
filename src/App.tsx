755229e3ed678f8f7ff23e42f63b587616ca0b53
sacasalv <sacasalv@gmail.com>
2026-01-29 20:38:59 +0100

    refactor(ui): Cambia nombre del componente por uno mas descriptivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
-import { Footer } from "./components/Footer";
+import { MobilePreview } from "./components/MobilePreview";
 
 function App() {
   return (
     <div className="bg-primary d-flex content-start justify-center min-h-screen p-4">
       <Hero />
       <Projects />
-      <Footer className="mt-auto fixed bottom-0 left-0 w-full"/>
+      <MobilePreview/>
     </div>
   );
 }
