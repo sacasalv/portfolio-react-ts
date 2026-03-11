5c741a9f41ee38a2b59505c3c6a571ebcbaa352a
sacasalv <sacasalv@gmail.com>
2026-03-11 20:45:05 +0100

    refactor: Cambia la extension del archivo y modifica la constante de iconos

--- src/components/MobilePreview.jsx
+++ src/components/MobilePreview.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 const ICONOS = [
-  "instagram.png",
-  "linkedin.png",
-  "twitter.png",
-  "github.png",
-  "gmail.png",
-];
+  { src: "instagram.png", alt: "Instagram" },
+  { src: "linkedin.png", alt: "LinkedIn" },
+  { src: "twitter.png", alt: "Twitter" },
+  { src: "github.png", alt: "GitHub" },
+  { src: "gmail.png", alt: "Gmail" },
+]
 
 export function MobilePreview() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -16,18 +16,19 @@ export function MobilePreview() {
       className={`fixed bottom-0 left-0 w-2xs h-96 ms-40 transition-transform duration-700 ease-out cursor-pointer ${isExpanded ? "translate-y-0" : "translate-y-80"} border-t-6 border-l-6 border-r-6 rounded-t-[50px] p-2 border-black bg-[url(./../public/images/fondo-movil.jpg)] bg-center bg-origin-content bg-content bg-no-repeat`}
       onClick={() => setIsExpanded(!isExpanded)}
     >
-      <div className="bg-white/1 backdrop-blur-[2px] h-full rounded-t-[30px] grid grid-cols-3 gap-2">
+      <div className="bg-white/1 backdrop-blur-[2px] h-full rounded-t-[30px] grid grid-cols-1 gap-2">
         <div className="absolute top-0 left-21 bg-black border-black-100 rounded-2xl w-25 h-8"></div>
-        {ICONOS.map((i) => (
-          <div key={i} className="mt-20">
-            <img
-              src={`/iconos/redesSociales/${i}`}
-              alt="Iconos"
-              className="inline-block size-15"
-            />
-          </div>
-        ))}
-        
+        <div className="h-full w-full grid grid-cols-3 mt-15">
+          {ICONOS.map(({src, alt}) => (
+            <div key={alt}>
+              <img
+                src={`/iconos/redesSociales/${src}`}
+                alt={alt}
+                className="inline-block size-15"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
